5002c570576751282391ccb772d134ed1cb512f8
Jo <134626490+Jcait@users.noreply.github.com>
2024-08-09 13:53:04 +0100

    add api for form

--- index.js
+++ index.js
@@ -9,3 +9,21 @@ async function getWeather(location) {
   console.log(response);
   console.log(weather.days[0].temp);
 }
+
+async function getCountries() {
+  const response = await fetch(
+    "https://parseapi.back4app.com/classes/list_of_eu_union_countries?limit=10&order=country&keys=country,capital'",
+    {
+      headers: {
+        "X-Parse-Application-Id": "7KffPh4njNmLBMB30kk9pvPGzBe7hwuK9SSQch6F", // This is the fake app's application id
+        "X-Parse-Master-Key": "5dN5MVGWctV1CIDY4CmI6AC6LGQImCj8DlpEI2Ix", // This is the fake app's readonly master key
+      },
+    }
+  );
+  const data = await response.json();
+  console.log(data.results);
+
+  data.results.forEach((country) => {
+    console.log(country.country);
+  });
+}
